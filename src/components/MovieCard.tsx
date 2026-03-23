0b94d9bec147ac1dac1dc5a84b0485cb47763559
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-23 13:37:08 +0900

    feat: fetch movies posters

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -10,7 +10,7 @@ function MovieCard({ movie }: MovieCardProps) {
 
     return(
         <div className="movie-card">
-            <img src={movie.poster_path} alt={movie.title} className="movie-img"/>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="movie-img"/>
             <div className="movie-info">
                 <p className="movie-title">Movie title: {movie.title}</p>
                 <p className="release-date">Release date: {movie.release_date}</p>
