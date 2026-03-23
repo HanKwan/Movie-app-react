36ce106876a796471a445e5ee73edbebb952d6f4
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-23 14:44:22 +0900

    feat: added navbar with home and fav routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Favorites from "./pages/Favorites"
+import Navbar from "./components/Navbar"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <main>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/favorites" element={<Favorites />}/>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import "../css/Navbar.css"
+
 function Navbar() {
     return(
-        <div className="nav-bar">
-            
-        </div>
+        <nav className="nav-wrapper">
+            <div className="nav-bar">
+            <div className="nav-brand">
+                <Link to="/">Movie app</Link>
+            </div>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/favorites">Favorites</Link>
+            </div>
+            </div>
+            </nav>
     )
 }
+
+export default Navbar
