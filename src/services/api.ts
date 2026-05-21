ec639997d72030fb9433a8002ba565d7afd36905
Han Kwan Saing <saihankwansaing@gmail.com>
2026-05-22 00:57:31 +0900

    refactor: return consistant data when fetching api instead of returning raw api

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,17 +5,31 @@ export const getPopularMovies = async(page: number) => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`)
     const data = await response.json()
 
-    return data
+    return {
+        results: data.results ?? [],            // returing like this instead of "return data"
+        total_pages: data.total_pages ?? 0,     // help prevent if it's return undefined
+        page: data.page ?? 1                    // give consistent behavior
+    }
 }
 
 export const getSearchedMovies = async(searchQuery: string, page: number) => {
     const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${page}`)
     const data = await res.json()
-    return data
+
+    return {
+        results: data.results ?? [],
+        total_pages: data.total_pages ?? 0,
+        page: data.page ?? 1
+    }
 }
 
 export const getMovieDetails = async(id: string) => {
     const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
     const data = await res.json()
-    return data     // data.results return list of movies, data return one movie
+
+    return {
+        results: data.results ?? [],
+        total_pages: data.total_pages ?? 0,
+        page: data.page ?? 1
+    }     
 }
